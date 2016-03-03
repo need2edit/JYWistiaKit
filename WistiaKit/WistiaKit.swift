6c5f36d2bc9f643284961de41e2f567207da888b
Jake Young <need2edit@gmail.com>
2016-03-02 23:05:41 -0500

    Stubbed in some base request functions and debugging

--- WistiaKit/WistiaKit.swift
+++ WistiaKit/WistiaKit.swift
@@ -14,6 +14,56 @@ We'll use Wistia's wonderful Data API as our bluprint.
 
 */
 
+
+// TODO: Using type aliases for now until we have concrete objects to work with
+public typealias Project = String
+public typealias Media = String
+public typealias Asset = String
+
+/// The object to encapsulate Wistia related functions and data.
+public class Wistia {
+    static var sharedInstance: Wistia = Wistia()
+    
+    /// The global API Password for singleton use
+    static var api_password: String {
+        return sharedInstance.api_password
+    }
+    
+    /// The global API Password for singleton use
+    static var numberOfProjects: Int {
+        return sharedInstance.projects.count
+    }
+    
+    static var numberOfMedias: Int {
+        return sharedInstance.medias.count
+    }
+    
+    var projects: [Project] = []
+    var medias: [Media] = []
+    
+    /// API Password for a given instance
+    private var api_password: String = ""
+}
+
+public func setup(api_password: String) {
+    Wistia.sharedInstance.api_password = api_password
+}
+
 public func request(route: DataAPI.Router) {
+    
+    if Wistia.api_password.isEmpty  {
+        print("You have not set an API Password")
+        print("Projects: \(Wistia.numberOfProjects)")
+        print("Medias: \(Wistia.numberOfMedias)")
+        
+        print("You have not set an API Password... aborting")
+        return
+    } else {
+        print(route.URL)
+    }
+    
+}
+
+public func List(route: DataAPI.Router) {
     print(route.URL)
 }

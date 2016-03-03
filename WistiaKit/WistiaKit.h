5d786d23e5e050d47dc7a65aadb777c1bb26e2e6
Jake Young <need2edit@gmail.com>
2016-03-02 21:36:38 -0500

    Initial Commit

---
+++ WistiaKit/WistiaKit.h
@@ -0,0 +1,19 @@
+//
+//  WistiaKit.h
+//  WistiaKit
+//
+//  Created by Jake Young on 3/2/16.
+//  Copyright © 2016 Jake Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WistiaKit.
+FOUNDATION_EXPORT double WistiaKitVersionNumber;
+
+//! Project version string for WistiaKit.
+FOUNDATION_EXPORT const unsigned char WistiaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WistiaKit/PublicHeader.h>
+
+

ac4de49ba6193c1473b4ef5b97b0c373e9f086a8
Jake Young <need2edit@gmail.com>
2016-04-15 17:30:43 -0400

    Cleaned out extraneous files from extra target additions

---
+++ iOS Example/iOS Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Example
+//
+//  Created by Jake Young on 4/15/16.
+//  Copyright © 2016 Jake Young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
